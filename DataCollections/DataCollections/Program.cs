df43987dde8d90f06b22d2b2e7be4272e01f2ccc
ForTakEr <simotonu.prees@tptlive.ee>
2018-02-15 08:57:46 +0200

    Oke

--- DataCollections/DataCollections/Program.cs
+++ DataCollections/DataCollections/Program.cs
@@ -10,6 +10,7 @@ namespace DataCollections
     {
         static void Main(string[] args)
         {
+            #region List<>
             //var cars = new List<string>();
 
             //var car1 = "BMW";
@@ -98,57 +99,134 @@ namespace DataCollections
 
 
 
+            #endregion
 
+            #region Dictionary
             //Dictionary
 
-            var openWith = new Dictionary<string, string>
-            { //   Key      Value
-                { "txt", "notepad.exe" },
-                { "bmp", "paint.exe" },
-                { "jpg", "paint.exe" },
-                { "rtf", "wordpad.exe" },
-            };
+            //var openWith = new Dictionary<string, string>
+            //{ //   Key      Value
+            //    { "txt", "notepad.exe" },
+            //    { "bmp", "paint.exe" },
+            //    { "jpg", "paint.exe" },
+            //    { "rtf", "wordpad.exe" },
+            //};
 
-            openWith["rtf"] = "winword.exe";
+            //openWith["rtf"] = "winword.exe";
 
-            try
-            {
-                openWith.Add("txt", "word.exe");
-            }
-            catch (ArgumentException)
-            {
+            //try
+            //{
+            //    openWith.Add("txt", "word.exe");
+            //}
+            //catch (ArgumentException)
+            //{
 
-                Console.WriteLine("An element with the key \"txt\" already exists!");
+            //    Console.WriteLine("An element with the key \"txt\" already exists!");
+            //}
+
+            //Console.WriteLine("For key = rtf, value = {0} ", openWith["rtf"]);
+            //Console.WriteLine("For key = bmp, value = {0}", openWith["bmp"]);
+            //Console.WriteLine("For key = txt, value = {0}", openWith["txt"]);
+
+            //try
+            //{
+            //    Console.WriteLine("For key = tif, value = {0}", openWith["tif"]);
+            //}
+            //catch (KeyNotFoundException)
+            //{
+            //    Console.WriteLine("Key = 'tif' is not found");
+            //}
+
+            //string value = "";
+            //if (openWith.TryGetValue("tif", out value))
+            //{
+            //    Console.WriteLine("For key = 'tif', value = {0}", value);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Key = 'tif' is not found");
+            //}
+
+            //if (!openWith.ContainsKey("ht"))
+            //{
+            //    openWith.Add("ht", "hyperterm.exe");
+            //    Console.WriteLine("Value added for key 'ht': {0}", openWith["ht"]);
+            //}
+
+            //foreach (KeyValuePair<string, string> kvp in openWith)
+            //{
+            //    Console.WriteLine("Key = {0} , Value = {1}", kvp.Key, kvp.Value);
+            //}
+
+
+            //Dictionary<string, string>.ValueCollection valueCollection = openWith.Values;
+
+            //foreach (string item in valueCollection)
+            //{
+            //    Console.WriteLine("Value = {0}", item);
+            //}
+
+            //Dictionary<string, string>.KeyCollection keyCollection = openWith.Keys;
+
+            //foreach (var item in valueCollection)
+            //{
+            //    Console.WriteLine("Key = {0}", item);
+            //}
+
+            //openWith.Remove("doc");
+            //if (!openWith.ContainsKey("doc"))
+            //{
+            //    Console.WriteLine("Key 'doc' is not found");
+            //}
+
+            #endregion
+
+
+            #region Queue
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("One");
+            numbers.Enqueue("Two");
+            numbers.Enqueue("Three");
+            numbers.Enqueue("Four");
+            numbers.Enqueue("Five");
+
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
             }
 
-            Console.WriteLine("For key = rtf, value = {0} ", openWith["rtf"]);
-            Console.WriteLine("For key = bmp, value = {0}", openWith["bmp"]);
-            Console.WriteLine("For key = txt, value = {0}", openWith["txt"]);
+            Console.WriteLine();
 
-            try
-            {
-                Console.WriteLine("For key = tif, value = {0}", openWith["tif"]);
-            }
-            catch (KeyNotFoundException)
-            {
-                Console.WriteLine("Key = 'tif' is not found");
-            }
+            Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
 
-            string value = "";
-            if (openWith.TryGetValue("tif", out value))
+            Console.WriteLine("Peek at the next item to dequeue: {0}", numbers.Peek());
+
+            Console.WriteLine("Dequeing '{0}'\n", numbers.Dequeue());
+
+            Queue<string> queueCopy = new Queue<string>(numbers.ToArray());
+            Console.WriteLine("Contents of the first copy:");
+            foreach (string number in queueCopy)
             {
-                Console.WriteLine("For key = 'tif', value = {0}", value);
-            }
-            else
-            {
-                Console.WriteLine("Key = 'tif' is not found");
+                Console.WriteLine(number);
             }
 
-            if (!openWith.ContainsKey("ht"))
+            string[] array2 = new string[numbers.Count * 2];
+            numbers.CopyTo(array2, numbers.Count);
+
+            Queue<string> queueCopy2 = new Queue<string>(array2);
+            Console.WriteLine("\nContents of the second Copy, with duplicates and nulls");
+            foreach (string number in queueCopy2)
             {
-                openWith.Add("ht", "hyperterm.exe");
-                Console.WriteLine("Value added for key 'ht': {0}", openWith["ht"]);
+                Console.WriteLine(number);
             }
+            Console.WriteLine("\nQueueCopy contains 'Four' = {0}", queueCopy.Contains("Four"));
+            queueCopy.Clear();
+            Console.WriteLine("\nqueueCopy count is: {0}", queueCopy.Count);
+            
+
+
+
+            #endregion
 
             Console.ReadLine();
         }
